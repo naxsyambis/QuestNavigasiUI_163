9f1ddbfefbd018d2ff91a7e7cb3c31958ef95460
naxsyambis <naxsyambis@gmail.com>
2025-11-07 15:49:30 +0700

    membuat garis berwarna merah

--- app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
@@ -72,6 +72,12 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
         }
     }
 }
