ff2c3e0154d0c230d55a614144b81b30017a275a
naxsyambis <naxsyambis@gmail.com>
2025-11-07 15:44:04 +0700

    membuat outlinedtextfiled

--- app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
@@ -4,6 +4,7 @@ import com.example.questnavigasiui_163.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -17,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,6 +42,15 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+
+                onValueChange = {},)
 
         }
     }
