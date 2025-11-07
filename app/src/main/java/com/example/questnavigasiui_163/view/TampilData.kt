9ae9e156fe1f35b8418cf429ca668181f81eee89
naxsyambis <naxsyambis@gmail.com>
2025-11-07 16:12:14 +0700

    Menambahakan val item

--- app/src/main/java/com/example/questnavigasiui_163/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_163/view/TampilData.kt
@@ -2,11 +2,17 @@ package com.example.questnavigasiui_163.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_163.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:() ->Unit
 ){
-
+    val item = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
 }
