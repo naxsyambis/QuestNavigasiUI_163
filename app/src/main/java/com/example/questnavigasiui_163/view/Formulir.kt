b0bf6e1b845cd0c8139a84b8677048e39c10331b
naxsyambis <naxsyambis@gmail.com>
2025-11-07 15:57:54 +0700

    membuat button click

--- app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_163/view/Formulir.kt
@@ -4,8 +4,12 @@ import com.example.questnavigasiui_163.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
@@ -86,6 +90,12 @@ fun FormIsian(
                 label = {Text(text = "Alamat")},
                 onValueChange = {},
             )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f)
+                    .padding(all=25.dp),
+                onClick = OnSubmitBtnClick
+            ){}
         }
     }
 }
