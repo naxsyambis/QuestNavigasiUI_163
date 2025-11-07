2fb6b24993e503cb6443546fe55ef6b448f240a2
naxsyambis <naxsyambis@gmail.com>
2025-11-07 16:19:55 +0700

    membuat top app bar

--- app/src/main/java/com/example/questnavigasiui_163/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_163/view/TampilData.kt
@@ -1,7 +1,16 @@
 package com.example.questnavigasiui_163.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.questnavigasiui_163.R
 
@@ -15,4 +24,14 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }
+        ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang)){}
+    }
 }
